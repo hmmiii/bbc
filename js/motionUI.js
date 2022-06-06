a74dfbb49f389e12a26c4e9ca3c68967c42f747c
hmmi <hmmi@kakao.com>
2022-06-07 01:28:25 +0900

    rollingNumber 플러그인 기능추가

--- js/motionUI.js
+++ js/motionUI.js
@@ -1,34 +1,50 @@
 (function($) {
-    $.fn.rollingNumbers = function(){
-        var length = 0;
-        var numbers = '';
-        var number = 0;
-        var count = null;
-        var target = this;
-        length = this.text().length;
-        numbers = this.text();
-        this.text('');
-    
-        this.addClass('rollingNumber');
+    $.fn.rollingNumbers = function(options){
+        const el = this;
+        const countTemp = '<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>';
+        let length = el.text().length;
+        let numbers = el.text();
+        let number = 0;
+        let count = null;
+        let rolling = null;
+        let nan = '';
+
+
+        let settings = $.extend({
+            commma : true
+        },options);
+
+
+        if(settings.commma === true){
+            el.text(parseInt(el.text()).toLocaleString('en'));
+            numbers = el.text();
+            length = el.text().length;
+        }
+
+
+        el.text('');
+        el.addClass('rollingNumber');
     
         
         for(let i=0; i<length; i++){
-            number = parseInt(numbers.substr(i,1));
+            number = numbers.substr(i,1);
             if(!isNaN(number)){
-                this.append('<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>');
-            }else {
-                number = numbers.substr(i,1);
-                this.append('<div class="nan">'+number+'</div>');
+                el.append(countTemp);
+            }
+            else {
+                nan = numbers.substr(i,1);
+                el.append('<div class="nan">'+nan+'</div>');
             }
         }
-        setTimeout(function(){
+
+        rolling = setTimeout(function(){
             for(let i=0; i<length; i++){
-                count = $(target).children('div').eq(i);
+                count = $(el).children('div').eq(i);
                 number = parseInt(numbers.substr(i,1));
                 if(!isNaN(number)){
                     count.attr('class','count'+number);
                 }
             }
-        },0);
+        });
     };
 }(jQuery));
