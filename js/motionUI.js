1c4d45dd4d8e83b9041f38e6a456a776d4c2f2f9
hmmi <hmmi@kakao.com>
2022-06-07 01:49:04 +0900

    unitTemp 삭제

--- js/motionUI.js
+++ js/motionUI.js
@@ -6,7 +6,6 @@
 
         const el = this;
         const countTemp = '<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>';
-        const unitTemp = '<div class="unit">'+settings.unit+'</div>';
         let length = el.text().length;
         let numbers = el.text();
         let number = 0;
