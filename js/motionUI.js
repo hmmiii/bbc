0509ea6c84bdde06c859721ada9f8ac1759af269
hmmi <hmmi@kakao.com>
2022-06-09 12:37:16 +0900

    플러그인 수정
    ready안에 넣음

--- js/motionUI.js
+++ js/motionUI.js
@@ -1,51 +1,52 @@
-(function($) {
-    $.fn.rollingNumbers = function(options){
-        let settings = $.extend({
-            comma : true
-        },options);
+$(document).ready(function(){
+    $.fn.rollingNumbers();
+});
 
-        const el = this;
-        const countTemp = '<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>';
-        let length = el.text().length;
-        let numbers = el.text();
-        let number = 0;
-        let count = null;
-        let nan = '';
+$.fn.rollingNumbers = function(options){
+    let settings = $.extend({
+        comma : true
+    },options);
 
-
-        if(settings.comma === true){
-            el.text(el.text().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-            numbers = el.text();
-            length = el.text().length;
-        }
+    const el = this;
+    const countTemp = '<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>';
+    let length = el.text().length;
+    let numbers = el.text();
+    let number = 0;
+    let count = null;
+    let nan = '';
 
 
-        el.text('');
-        el.addClass('rollingNumber');
-    
-        (function(){
-            for(let i=0; i<length; i++){
-                number = numbers.substr(i,1);
-                if(!isNaN(number)){
-                    el.append(countTemp);
-                }
-                else {
-                    nan = numbers.substr(i,1);
-                    el.append('<div class="nan">'+nan+'</div>');
-                }
+    if(settings.comma === true){
+        el.text(el.text().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+        numbers = el.text();
+        length = el.text().length;
+    }
+
+
+    el.text('');
+    el.addClass('rollingNumber');
+
+    (function(){
+        for(let i=0; i<length; i++){
+            number = numbers.substr(i,1);
+            if(!isNaN(number)){
+                el.append(countTemp);
             }
-            (function(){
-                setTimeout(function(){
-                    for(let i=0; i<length; i++){
-                        count = $(el).children('div').eq(i);
-                        number = parseInt(numbers.substr(i,1));
-                        if(!isNaN(number)){
-                            count.attr('class','count'+number);
-                        }
+            else {
+                nan = numbers.substr(i,1);
+                el.append('<div class="nan">'+nan+'</div>');
+            }
+        }
+        (function(){
+            setTimeout(function(){
+                for(let i=0; i<length; i++){
+                    count = $(el).children('div').eq(i);
+                    number = parseInt(numbers.substr(i,1));
+                    if(!isNaN(number)){
+                        count.attr('class','count'+number);
                     }
-                })
-            })();
+                }
+            })
         })();
-
-    };
-}(jQuery));
+    })();
+};
