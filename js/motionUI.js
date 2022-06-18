e387858108b18d6d6680fb1f4cfc17c6154b3c29
hmmi <hmmi@kakao.com>
2022-06-19 01:38:33 +0900

    rollingNumbers 플러그인 개선 작업
    data- 사용 옵션 추가

--- js/motionUI.js
+++ js/motionUI.js
@@ -2,21 +2,32 @@
     $.fn.rollingNumbers = function(options){
         let settings = $.extend({
             comma : true,
-            delay : 200
+            delay : 200,
+            data : true
         },options);
     
         const el = this;
         const countTemp = '<div class="count"><span>9</span><span>8</span><span>7</span><span>6</span><span>5</span><span>4</span><span>3</span><span>2</span><span>1</span><span>0</span></div>';
         let length = el.text().length;
-        let numbers = el.text();
+        let numbers = '';
         let number = 0;
         let count = null;
         let nan = '';
     
-    
-        if(settings.comma === true){
-            el.text(el.text().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+        if(settings.data === true){
+            numbers = el.attr('data-rollingNumbers');
+        }else{
             numbers = el.text();
+        }
+
+        if(settings.comma === true){
+            if(settings.data === true){
+                numbers = numbers.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+                el.text(numbers)
+            }else{
+                el.text(el.text().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+                numbers = el.text();
+            }
             length = el.text().length;
         }
     
